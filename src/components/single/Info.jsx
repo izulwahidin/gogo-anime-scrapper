3a4d6b9aa48c76360aad94be90afecdc1402906a
Wahidin <izulwahidin@my.id>
2023-01-16 21:11:27 +0700

    aspec ratio

--- src/components/single/Info.jsx
+++ src/components/single/Info.jsx
@@ -14,7 +14,7 @@ export default function Info({data,shimmer}) {
                     <>
                         <div className='m-2 p-3'>
                         <h2 className='text-2xl pb-5'>{data.full_title}</h2>
-                        <iframe className="w-full aspect-video rounded" frameborder="0" src={data.video} title={data.title}></iframe>
+                        <iframe className="w-full h-full aspect-video rounded" frameborder="0" src={data.video} title={data.title}></iframe>
                         <p className='font-bold py-2'>{data.title}</p>
                         <p className='text-sm '>{data.description}</p>
                         </div>
